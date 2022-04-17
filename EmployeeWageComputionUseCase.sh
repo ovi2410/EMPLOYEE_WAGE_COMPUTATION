86169a17288a7434d478ff58360d22c1dad7a89a
ovi2410 <guneshwarifegade@gmail.com>
2022-04-17 14:56:53 +0530

    modified:calculate to wage

--- EmployeeWageComputionUseCase.sh
+++ EmployeeWageComputionUseCase.sh
@@ -15,3 +15,12 @@ then
 else
 	echo "Employee Absent"
 fi
+
+#TO FUNCTION CALCULATE EMPLOYEE WAGE
+function calculateDailyEmployeeWage(){
+	read -p "Assume Wage per hour is :" isWagePerHour "rupees"
+	read -p "To full day hours is :" isFullDayHour
+	calculateEmployeeWage=$(( $isWagePerHour * $isFullDayHour ))
+	echo "To calculate daily employee wage :" $calculateEmployeeWage "rupees"
+}
+calculateDailyEmployeeWage
